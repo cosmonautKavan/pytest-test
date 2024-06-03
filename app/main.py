3bddd783291e7fc82d2cb98ed04c43ae8296ada3
cosmonautKavan <kavan.gondalia@cosmocloud.io>
2024-06-03 12:38:25 +0530

    fix: remove forevor loop in main.py file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import asyncio
 import time
 
+
 async def fetch_data():
     asyncio.sleep(1)
     return {"data": "sample data"}
@@ -9,8 +10,8 @@ async def fetch_data():
 def process_data(data):
     return data["data"].upper()
 
-count = 0
-while True:
+
+for count in range(10):
     print(process_data({"data": "sample data"}), count)
     count += 1
     time.sleep(1)
